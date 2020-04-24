b2e25b34843a94a3b033185c5220ede642bfef79
rcomito@uoregon.edu <rcomito@uoregon.edu>
2020-04-23 19:11:07 -0700

    Coding Practice

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,49 @@
 // jshint esversion: 6
 
+//req 3a
+let revWords1 = (s) => {
+  let result = "";
+  let reverse = "";
+  for (var i = 0; i < s.length; i++) {
+    reverse = s[i] + reverse
+  }
+  let arr = reverse.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    result = arr[i] + " " + result;
+  }
+  return result;
+};
+
+//req. 3b
+let revWords2 = (str) => {
+  let result = "";
+  let arr = str.split("");
+  let reverse = "";
+  arr.forEach((char)=>{
+    reverse = char + reverse;
+  })
+  let flip = reverse.split(" ");
+  flip.forEach((char)=>{
+    result = char + " " + result;
+  })
+  return result;
+};
+
+//req. 3c
+let revWords3 = (str) => {
+  let result = "";
+  let reverse = "";
+for (char of str){
+  reverse = char + reverse;
+}
+let arr = reverse.split(" ");
+for (char of arr){
+  result = char + " " + result;
+}
+return result;
+};
+
+//extra credit
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
